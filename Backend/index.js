b65a9bbd1038018740fd51f612f53e1fecbab6d8
Ritik Sachan <ritiksachan@Ritiks-MacBook-Air.local>
2023-08-23 12:44:23 +0530

    basic api completed

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
@@ -8,10 +9,52 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const port = process.env.PORT;
+app.get("/trains", async (req, res) => {
+  const data = {
+    companyName: "Straindules",
+    clientID: "5a9052cc-f989-4980-b532-b9824e37c08e",
+    ownerName: "Ritik",
+    ownerEmail: "ritiksachan927@gmail.com",
+    rollNo: "2000970100084",
+    clientSecret: "wUhuIAVSudzskSci",
+  };
+  const authApiResponse = await axios.post(
+    "http://20.244.56.144/train/auth",
+    data
+  );
 
-app.get("/", (req, res) => {
-  res.json({ msg: "Hello World!" });
+  const authToken = authApiResponse.data.access_token;
+
+  const response = await axios.get("http://20.244.56.144/train/trains", {
+    headers: { Authorization: "Bearer " + authToken },
+  });
+  res.send(response.data);
+});
+
+app.get("/trains/:trainNumber", async (req, res) => {
+  const trainNumber = req.params.trainNumber;
+  const data = {
+    companyName: "Straindules",
+    clientID: "5a9052cc-f989-4980-b532-b9824e37c08e",
+    ownerName: "Ritik",
+    ownerEmail: "ritiksachan927@gmail.com",
+    rollNo: "2000970100084",
+    clientSecret: "wUhuIAVSudzskSci",
+  };
+  const authApiResponse = await axios.post(
+    "http://20.244.56.144/train/auth",
+    data
+  );
+
+  const authToken = authApiResponse.data.access_token;
+
+  const response = await axios.get(
+    "http://20.244.56.144/train/trains/" + trainNumber,
+    {
+      headers: { Authorization: "Bearer " + authToken },
+    }
+  );
+  res.send(response.data);
 });
 
 app.listen(PORT, () => {
